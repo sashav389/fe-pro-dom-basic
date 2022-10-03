b48e25f2fcfcbcd2ab1dcc1c22ad964cc7034912
Sasha Vasylenko <svasilenko389@gmail.com>
2022-10-03 23:02:14 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+    document.querySelectorAll(`li:nth-child(2n)`)
+        .forEach(li => li.style.backgroundColor = `red`)
+};
 
-export const findElement = () => {};
+export const findElement = () => {
+    let li = document.querySelector(`li`);
+    while (!li.classList.contains(`likedItem`)) {
+        li = li.nextElementSibling;
+    }
+    li.style.backgroundColor = `blue`;
+};
